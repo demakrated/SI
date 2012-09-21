1e4421233650b396198ac0d0ee6e3ca28574677b
demakrated <danielpedrajasvandevelde@gmail.com>
2012-09-22 00:40:56 +0200

    Añadida funcionalidad heurística a getCoste que se lanza al escoger el nodo de listafrontera

--- Practica1SI2012Alumno/src/practica1si2012/TNodo.java
+++ Practica1SI2012Alumno/src/practica1si2012/TNodo.java
@@ -14,38 +14,45 @@ public class TNodo {
     private int fila = 0;
     private int columna = 0;
     private int coste = 0;
-    private int orden = 1;
+    private int real = 0;
+    private Heuristica h;
     private TNodo daddy;    //puntero para no perder el padre y poder reconstruir el camino
     
     //constructor por parametros
-    TNodo(int x, int y, int coste, int orden, TNodo papa){
-        fila = x; columna = y; daddy = papa; this.coste = coste; this.orden = orden;
+    TNodo(int x, int y, int real, TNodo papa, Heuristica h){
+        fila = x; columna = y; daddy = papa; this.h = h; this.real = real; coste = -1;
+    }
+    
+    TNodo(int x, int y, int real, TNodo papa){
+        fila = x; columna = y; daddy = papa; this.real = real; coste = -1;
     }
     
     //devuelve vector con los nodos expandidos (arriba, abajo, izq, dxa)
     public ArrayList<TNodo> expandir(int [][] mundo){
         
         //array de los nodos a los que se puede expandir
-        ArrayList <TNodo> expansion = new ArrayList <TNodo>();        
+        ArrayList <TNodo> expansion = new ArrayList <TNodo>();
+        
+        TNodo aux;
         
         //si puedo expandir hacia arriba expando
         if(fila > 1 && mundo[fila - 1][columna] == 0){
-            expansion.add(new TNodo(fila-1, columna, coste + 1, orden + 1, this));
+            expansion.add(new TNodo(fila-1, columna, real + 1, this, h));
             //System.out.println("expande arriba");
         }
         //si me puedo expandir hacia la izquierda
         if(columna > 1 && mundo[fila][columna - 1] == 0){
-            expansion.add(new TNodo(fila, columna-1,coste + 1, orden+1, this));
+            expansion.add(new TNodo(fila, columna-1, real + 1, this, h));
             //System.out.println("expande izq");
         }
         //si me puedo expandir derecha
         if(columna + 1 < mundo.length - 1 && mundo[fila][columna + 1] == 0){
-            expansion.add(new TNodo(fila, columna+1,coste + 1, orden + 1, this));
+            expansion.add(new TNodo(fila, columna+1, real + 1, this, h));
             //System.out.println("expande dxa");
         }
         //expansion hacia abajo
         if(fila + 1 < mundo.length - 1 && mundo[fila + 1][columna] == 0){
-            expansion.add(new TNodo(fila + 1, columna,coste + 1, orden + 1, this));
+            expansion.add(new TNodo(fila + 1, columna, real + 1, this, h));
             //System.out.println("expande abajo");
         }
         
@@ -55,9 +62,9 @@ public class TNodo {
     //funcion para comparar pesos de nodos
     public int comparador(TNodo b){
         
-        if(this.coste < b.coste){
+        if(this.getCoste() < b.getCoste()){
             return -1;}
-        else if(this.coste == b.coste){
+        else if(this.getCoste() == b.getCoste()){
             return 0;}
         else{
             return 1;
@@ -66,6 +73,7 @@ public class TNodo {
     
     //getter coste
     public int getCoste(){
+        if(coste<0) coste = real + h.calcular(this);
         return coste;
     }
     
@@ -95,13 +103,5 @@ public class TNodo {
         return daddy;
     }
     
-    //getter de orden
-    public int getOrden(){
-        return orden;
-    }
-    
-    //setter del orden de visita
-    public void setOrden(int orden){
-        this.orden = orden;
-    }
+
 }
